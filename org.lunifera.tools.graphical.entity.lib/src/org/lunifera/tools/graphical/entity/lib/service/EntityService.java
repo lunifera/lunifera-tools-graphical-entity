b947dc4c57e99bf0a30dfa00f9df6b817a238431
Florian Pirchner <florian.pirchner@gmail.com>
2015-06-12 15:02:46 +0200

    Fixes

--- org.lunifera.tools.graphical.entity.lib/src/org/lunifera/tools/graphical/entity/lib/service/EntityService.java
+++ org.lunifera.tools.graphical.entity.lib/src/org/lunifera/tools/graphical/entity/lib/service/EntityService.java
@@ -7,6 +7,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.ecore.EEnumLiteral;
@@ -38,6 +41,15 @@ import org.eclipse.sirius.tools.api.command.CommandContext;
 import org.eclipse.sirius.viewpoint.DRepresentation;
 import org.eclipse.sirius.viewpoint.DSemanticDecorator;
 import org.eclipse.sirius.viewpoint.ViewpointPackage;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.resource.XtextResource;
 import org.lunifera.dsl.semantic.common.helper.Bounds;
 import org.lunifera.dsl.semantic.common.types.LAttribute;
 import org.lunifera.dsl.semantic.common.types.LDataType;
@@ -930,6 +942,40 @@ public class EntityService {
 				lPkg.getImports().add(lImp);
 			}
 		}
+	}
+	
+	public EObject openTextEditor(EObject any) {
+		if (any != null && any.eResource() instanceof XtextResource
+				&& any.eResource().getURI() != null) {
+
+			String fileURI = any.eResource().getURI().toPlatformString(true);
+			IFile workspaceFile = ResourcesPlugin.getWorkspace().getRoot()
+					.getFile(new Path(fileURI));
+			if (workspaceFile != null) {
+				IWorkbenchPage page = PlatformUI.getWorkbench()
+						.getActiveWorkbenchWindow().getActivePage();
+				try {
+					IEditorPart openEditor = IDE.openEditor(page,
+							workspaceFile,
+							"org.lunifera.dsl.entity.xtext.EntityGrammar",
+							true);
+					if (openEditor instanceof AbstractTextEditor) {
+						ICompositeNode node = NodeModelUtils
+								.findActualNodeFor(any);
+						if (node != null) {
+							int offset = node.getOffset();
+							int length = node.getTotalEndOffset() - offset;
+							((AbstractTextEditor) openEditor).selectAndReveal(
+									offset, length);
+						}
+					}
+					// editorInput.
+				} catch (PartInitException e) {
+					// Put your exception handler here if you wish to.
+				}
+			}
+		}
+		return any;
 	}
 
 	private class ImportCollectionSwitch extends LunEntitySwitch<Boolean> {
